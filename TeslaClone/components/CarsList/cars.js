0b8996a2b7e6a9e66047d0ff73fa1780c97736e0
Özgür Deniz Güler <ozgurguler.deniz@gmail.com>
2022-09-03 00:10:06 +0300

    React Native ile Klon
    React Native ile Tesla Aplikasyonunun klonu oluşturuldu.

---
+++ TeslaClone/components/CarsList/cars.js
@@ -0,0 +1,20 @@
+export default [{
+  name: 'Model S',
+  tagline: 'Starting at $69,420',
+  image: require('../../assets/TeslaAssetts/Models.jpeg'),
+}, {
+  name: 'Model 3',
+  tagline: 'Order Online for',
+  taglineCTA: 'Touchless Delivery',
+  image: require('../../assets/TeslaAssetts/Model3.jpeg'),
+}, {
+  name: 'Model X',
+  tagline: 'Order Online for',
+  taglineCTA: 'Touchless Delivery',
+  image: require('../../assets/TeslaAssetts/ModelX.jpeg'),
+}, {
+  name: 'Model Y',
+  tagline: 'Order Online for',
+  taglineCTA: 'Touchless Delivery',
+  image: require('../../assets/TeslaAssetts/ModelY.jpeg'),
+}];
